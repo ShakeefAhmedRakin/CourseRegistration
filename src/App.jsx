827eb4de136bf761449ac8649d4af45a6e6a40da
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2023-09-16 16:52:16 +0600

    Created the json file containing all the course details and their image paths.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  // const [courses,setCourses] = useState [];
+
+  useEffect(() => {
+    fetch("courses.json")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <>
       <h1 className="text-center text-5xl text-red-500 ">HELLO</h1>
-      <button className="btn">Button</button>
     </>
   );
 }
