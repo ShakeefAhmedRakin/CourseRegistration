46a5d68b81f3001418254db181ea889d59f29d4d
Shakeef Ahmed Rakin <shakeef.rakin321@gmail.com>
2023-09-16 16:33:14 +0600

    Installed React with vite, tailwind, daisyui.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-5xl text-red-500 ">HELLO</h1>
+      <button className="btn">Button</button>
+    </>
+  );
+}
+
+export default App;
